c047e5b9c9b5cb21e50b4e31e01432aa52c9fce5
Wendell Bruno <wendell_101@hotmail.com>
2021-09-09 16:17:27 -0300

    modelo base para a maioria dos Projetos.

---
+++ modeloHtmlCss/assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+function calculadora(peso, altura){
+    const peso = document.querySelector('peso');
+    const altura = document.querySelector('altura');
+    const resultado = (peso*altura) / peso;
+
+    
+    alert(resultado);
+}
